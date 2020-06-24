069d09bfb0a1b4b9314996d9ba700e90a51efc71
Isaac Nam <ikn208@nyu.edu>
2020-06-24 16:26:51 -0400

    csv file update

--- src/scripts/barchart.js
+++ src/scripts/barchart.js
@@ -1,40 +1,83 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const height = 500;
-    const width = 940;
+    var data = [{"year": 2010, "loss":4500}, {"year": 2011, "loss":3000}, {"year": 2012, "loss":5000}, {"year": 2013, "loss":7500}, {"year": 2014, "loss":6000}];
+    var spacing = 120, width = 600, height = 500;
     
-    const margin = { y: 100 };
-    const svg = d3.select('#viewers-svg').append('svg')
-        .style('background', '#f1f1f1')
-        .attr('height', height)
-        .attr("transform", "translate(" + 0 + "," + margin.y + ")");
+    var svg = d3.select("#viewers-svg").append("svg")
+                .attr("width", width).attr("height", height)
+                .style("background", "pink")
+                .append("g")
+                .attr("transform", "translate(" + spacing/2 + "," + spacing/2 + ")");
+    
+    var xScale = d3.scaleLinear()
+                    .domain([d3.min(data, function(d){return d.year;}), d3.max(data, function(d){return d.year;})])
+                    .range([0, width-spacing]);
+    var yScale = d3.scaleLinear()
+                    .domain([2500, 8000])
+                    .range([height-spacing, 0]);
+    
+    var xAxis = d3.axisBottom(xScale)
+    .ticks(5).tickFormat(function(d){return d;});
+    var yAxis = d3.axisLeft(yScale)
+    .tickFormat(function(d){return "$" + d;});
+    
+    svg.append("g").attr("transform", "translate(30," + (height-spacing) + ")").call(xAxis);
+    svg.append("g").call(yAxis);
+    d3.select("g path").remove();
+    var rect = svg.selectAll("rect").data(data);
+    rect.enter().append("rect")
+        .attr("width", 30)
+        .attr("height", function(d){return (height-spacing) - yScale(d.loss);})
+        .attr("x", function(d){return xScale(d.year) + 15;})
+        .attr("y", function(d){return yScale(d.loss)})
+        .style("fill", "indigo");
 
-    d3.csv('http://localhost:8080/src/data/average-viewership.csv').then(function(data) {
-        const xScale = d3.scaleLinear()
-            .domain([2016, 2020])
-            .range(10000, 0);
-        const yScale = d3.scaleLinear()
-            .domain([0, d3.max(data, function(d){return +d.Viewers;})])
-            .range([0, 500]);
+    // const height = 500;
+    // const width = 940;
+    // const spacing = 100;
+    
+    // const margin = 100;
+    // const svg = d3.select('#viewers-svg').append('svg')
+    //     .style('background', '#f1f1f1')
+    //     .attr('height', height)
+    //     .attr("transform", "translate(" + 0 + "," + margin + ")")
+    //     .append('g')
+    //     .attr('transform', 'translate(' +
+    //     spacing/2 + ',' + spacing/2 + ')');
 
-        console.log();
+    // const data = [{"year": 2010, "loss":4500}, {"year": 2011, "loss":3000}, {"year": 2012, "loss":5000}, {"year": 2013, "loss":7500}, {"year": 2014, "loss":6000}];
 
-        const xAxis = d3.axisBottom(xScale)
-            .ticks(5).tickFormat(function(d, i){return d});
+    // const xScale = d3.scaleLinear()
+    //     .domain([
+    //         d3.min(data, function(d){return d.year;}), d3.max(data, function(d){return d.year;})
+    //     ])
+    //     .range(0, width);
 
-        const yAxis = d3.axisLeft(yScale)
-            .ticks(6).tickFormat(function(d, i) {return d});
+    // const xAxis = d3.axisBottom(xScale)
+    //     .ticks(5).tickFormat(function(d, i){return d});
 
-        d3.select("g path").remove();
+    // svg.append('g').attr("transform", "translate(30," + height-spacing + ")").call(xAxis);
 
-        svg.append('g').attr("transform", "translate(30," + 10 + ")").call(xAxis);
-        svg.append('g').call(yAxis);
-        const bars = svg.selectAll('rect').data(data);
-        bars.enter().append('rect').classed('bars', true)
-            .attr('height', function(d) {return yScale(+d.Viewers)})
-            .attr('x', function(d, i) {return i * 40;})
-            .attr('y', function(d) {return height - (yScale(+d.Viewers));})
-            .text(function(d){ return d.Viewers ;})
+    // d3.csv('http://localhost:8080/src/data/average-viewership.csv').then(function(data) {
+        
+        // const yScale = d3.scaleLinear()
+        //     .domain([0, d3.max(data, function(d){return +d.Viewers;})])
+        //     .range([0, height-spacing]);
 
-    });
+        // const yAxis = d3.axisLeft(yScale)
+        //     .ticks(6).tickFormat(function(d, i) {return d});
+
+        // d3.select("g path").remove();
+
+        // svg.append('g').attr("transform", "translate(30," + height-spacing + ")").call(xAxis);
+        // svg.append('g').call(yAxis);
+        // const bars = svg.selectAll('rect').data(data);
+        // bars.enter().append('rect').classed('bars', true)
+        //     .style('background', 'pink')
+        //     .attr('height', function(d) {return yScale(+d.Viewers)})
+        //     .attr('x', function(d, i) {return i * 40;})
+        //     .attr('y', function(d) {return height - (yScale(+d.Viewers));})
+        //     .text(function(d){ return d.Viewers ;})
+
+    // });
 
 })
